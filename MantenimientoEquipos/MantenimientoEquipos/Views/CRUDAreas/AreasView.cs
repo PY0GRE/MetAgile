645a31fa1e6f34d136f21ff969c884f48fd4fce5
Bryan Arturo Barraza Garcia <bry.art717@gmail.com>
2025-06-17 15:40:47 -0600

    Creación del flujo de Trabajo de los CRUD´s

---
+++ MantenimientoEquipos/MantenimientoEquipos/Views/CRUDAreas/AreasView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MantenimientoEquipos.Views.CRUDAreas
+{
+    public partial class AreasView : Form
+    {
+        public AreasView()
+        {
+            InitializeComponent();
+        }
+    }
+}
