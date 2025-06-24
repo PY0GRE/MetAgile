147a4209d6b1893c0cdabc18c336ef8088fb9fdd
Bryan Arturo Barraza Garcia <bry.art717@gmail.com>
2025-06-23 22:28:13 -0600

    Creación de Formularios restantes para la visualización completa

--- MantenimientoEquipos/MantenimientoEquipos/EstandarizacionComponentes.Designer.cs
+++ MantenimientoEquipos/MantenimientoEquipos/EstandarizacionComponentes.Designer.cs
@@ -112,7 +112,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(524, 500);
+            this.ClientSize = new System.Drawing.Size(509, 500);
             this.Controls.Add(this.estandarTitleLabel1);
             this.Controls.Add(this.estandarLabel1);
             this.Controls.Add(this.estandarLabel2);
