29396bc53d28a8fe5596f4aa9edb974dd84adab3
Your Name <you@example.com>
2025-06-26 22:26:29 -0600

    Agregué formulario de Usuarios con DataGridView y diseño visual

---
+++ MantenimientoEquipos/MantenimientoEquipos/Views/CRUDUsers/Usuarios.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace MantenimientoEquipos.Views.CRUDUsers
+{
+    partial class Usuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.estandarTitleLabel1 = new MantenimientoEquipos.ModifierComponents.EstandarTitleLabel();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // estandarTitleLabel1
+            // 
+            this.estandarTitleLabel1.AutoSize = true;
+            this.estandarTitleLabel1.Font = new System.Drawing.Font("Calibri", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.estandarTitleLabel1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.estandarTitleLabel1.Location = new System.Drawing.Point(44, 45);
+            this.estandarTitleLabel1.Margin = new System.Windows.Forms.Padding(10);
+            this.estandarTitleLabel1.Name = "estandarTitleLabel1";
+            this.estandarTitleLabel1.Size = new System.Drawing.Size(167, 49);
+            this.estandarTitleLabel1.TabIndex = 0;
+            this.estandarTitleLabel1.Text = "Usuarios";
+            this.estandarTitleLabel1.Click += new System.EventHandler(this.estandarTitleLabel1_Click);
+            // 
+            // dgvUsuarios
+            // 
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuarios.Location = new System.Drawing.Point(229, 191);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.Size = new System.Drawing.Size(240, 150);
+            this.dgvUsuarios.TabIndex = 1;
+            this.dgvUsuarios.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Usuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvUsuarios);
+            this.Controls.Add(this.estandarTitleLabel1);
+            this.Name = "Usuarios";
+            this.Text = "Usuarios";
+            this.Load += new System.EventHandler(this.Usuarios_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ModifierComponents.EstandarTitleLabel estandarTitleLabel1;
+        private System.Windows.Forms.DataGridView dgvUsuarios;
+    }
+}
