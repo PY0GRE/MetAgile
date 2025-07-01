a667479d47c58238431ce549fd963db16a2a68aa
KYNIGOS\ikerj <ikerjavier1219@gmail.com>
2025-06-30 23:15:35 -0600

    Eliminación de la pantalla Delete de mantenimientos y modificación visual del menú principal de mantenimientos.

--- MantenimientoEquipos/MantenimientoEquipos/Views/CRUDMaintenance/MenuMaintenance.Designer.cs
+++ MantenimientoEquipos/MantenimientoEquipos/Views/CRUDMaintenance/MenuMaintenance.Designer.cs
@@ -28,12 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.estandarTitleLabel1 = new MantenimientoEquipos.ModifierComponents.EstandarTitleLabel();
             this.estandarButton1 = new MantenimientoEquipos.ModifierComponents.EstandarButton();
             this.estandarButton2 = new MantenimientoEquipos.ModifierComponents.EstandarButton();
             this.estandarButton3 = new MantenimientoEquipos.ModifierComponents.EstandarButton();
-            this.estandarButton4 = new MantenimientoEquipos.ModifierComponents.EstandarButton();
             this.estandarButton5 = new MantenimientoEquipos.ModifierComponents.EstandarButton();
+            this.estandarDataView1 = new MantenimientoEquipos.ModifierComponents.EstandarDataView();
+            ((System.ComponentModel.ISupportInitialize)(this.estandarDataView1)).BeginInit();
             this.SuspendLayout();
             // 
             // estandarTitleLabel1
@@ -41,7 +45,7 @@
             this.estandarTitleLabel1.AutoSize = true;
             this.estandarTitleLabel1.Font = new System.Drawing.Font("Calibri", 25F, System.Drawing.FontStyle.Bold);
             this.estandarTitleLabel1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.estandarTitleLabel1.Location = new System.Drawing.Point(91, 19);
+            this.estandarTitleLabel1.Location = new System.Drawing.Point(107, 9);
             this.estandarTitleLabel1.Margin = new System.Windows.Forms.Padding(10);
             this.estandarTitleLabel1.Name = "estandarTitleLabel1";
             this.estandarTitleLabel1.Size = new System.Drawing.Size(614, 51);
@@ -55,11 +59,11 @@
             this.estandarButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.estandarButton1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.estandarButton1.ForeColor = System.Drawing.Color.White;
-            this.estandarButton1.Location = new System.Drawing.Point(100, 150);
+            this.estandarButton1.Location = new System.Drawing.Point(141, 93);
             this.estandarButton1.Name = "estandarButton1";
-            this.estandarButton1.Size = new System.Drawing.Size(240, 35);
+            this.estandarButton1.Size = new System.Drawing.Size(122, 35);
             this.estandarButton1.TabIndex = 1;
-            this.estandarButton1.Text = "Nuevo mantenimiento";
+            this.estandarButton1.Text = "Agregar";
             this.estandarButton1.UseVisualStyleBackColor = false;
             this.estandarButton1.Click += new System.EventHandler(this.estandarButton1_Click);
             // 
@@ -70,11 +74,11 @@
             this.estandarButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.estandarButton2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.estandarButton2.ForeColor = System.Drawing.Color.White;
-            this.estandarButton2.Location = new System.Drawing.Point(100, 250);
+            this.estandarButton2.Location = new System.Drawing.Point(269, 93);
             this.estandarButton2.Name = "estandarButton2";
-            this.estandarButton2.Size = new System.Drawing.Size(240, 35);
+            this.estandarButton2.Size = new System.Drawing.Size(122, 35);
             this.estandarButton2.TabIndex = 2;
-            this.estandarButton2.Text = "Ver mantenimientos";
+            this.estandarButton2.Text = "Ver";
             this.estandarButton2.UseVisualStyleBackColor = false;
             // 
             // estandarButton3
@@ -84,27 +88,13 @@
             this.estandarButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.estandarButton3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.estandarButton3.ForeColor = System.Drawing.Color.White;
-            this.estandarButton3.Location = new System.Drawing.Point(465, 150);
+            this.estandarButton3.Location = new System.Drawing.Point(13, 93);
             this.estandarButton3.Name = "estandarButton3";
-            this.estandarButton3.Size = new System.Drawing.Size(240, 35);
+            this.estandarButton3.Size = new System.Drawing.Size(122, 35);
             this.estandarButton3.TabIndex = 3;
-            this.estandarButton3.Text = "Editar mantenimiento";
+            this.estandarButton3.Text = "Actualizar";
             this.estandarButton3.UseVisualStyleBackColor = false;
             this.estandarButton3.Click += new System.EventHandler(this.estandarButton3_Click);
-            // 
-            // estandarButton4
-            // 
-            this.estandarButton4.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.estandarButton4.BorderRadius = 20;
-            this.estandarButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.estandarButton4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.estandarButton4.ForeColor = System.Drawing.Color.White;
-            this.estandarButton4.Location = new System.Drawing.Point(465, 250);
-            this.estandarButton4.Name = "estandarButton4";
-            this.estandarButton4.Size = new System.Drawing.Size(240, 35);
-            this.estandarButton4.TabIndex = 4;
-            this.estandarButton4.Text = "Eliminar mantenimiento";
-            this.estandarButton4.UseVisualStyleBackColor = false;
             // 
             // estandarButton5
             // 
@@ -113,26 +103,64 @@
             this.estandarButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.estandarButton5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.estandarButton5.ForeColor = System.Drawing.Color.White;
-            this.estandarButton5.Location = new System.Drawing.Point(517, 396);
+            this.estandarButton5.Location = new System.Drawing.Point(635, 425);
             this.estandarButton5.Name = "estandarButton5";
-            this.estandarButton5.Size = new System.Drawing.Size(267, 35);
+            this.estandarButton5.Size = new System.Drawing.Size(188, 35);
             this.estandarButton5.TabIndex = 5;
-            this.estandarButton5.Text = "Regresar al menú del sistema";
+            this.estandarButton5.Text = "Regresar al menú";
             this.estandarButton5.UseVisualStyleBackColor = false;
+            // 
+            // estandarDataView1
+            // 
+            this.estandarDataView1.AllowUserToAddRows = false;
+            this.estandarDataView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.estandarDataView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.estandarDataView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.estandarDataView1.BackgroundColor = System.Drawing.Color.White;
+            this.estandarDataView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.estandarDataView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.estandarDataView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.estandarDataView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Calibri", 10F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.LightBlue;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.estandarDataView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.estandarDataView1.EnableHeadersVisualStyles = false;
+            this.estandarDataView1.GridColor = System.Drawing.Color.LightGray;
+            this.estandarDataView1.Location = new System.Drawing.Point(13, 134);
+            this.estandarDataView1.Name = "estandarDataView1";
+            this.estandarDataView1.RowHeadersVisible = false;
+            this.estandarDataView1.RowHeadersWidth = 51;
+            this.estandarDataView1.RowTemplate.Height = 24;
+            this.estandarDataView1.Size = new System.Drawing.Size(810, 276);
+            this.estandarDataView1.TabIndex = 6;
             // 
             // MenuMaintenance
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(835, 472);
+            this.Controls.Add(this.estandarDataView1);
             this.Controls.Add(this.estandarButton5);
-            this.Controls.Add(this.estandarButton4);
             this.Controls.Add(this.estandarButton3);
             this.Controls.Add(this.estandarButton2);
             this.Controls.Add(this.estandarButton1);
             this.Controls.Add(this.estandarTitleLabel1);
             this.Name = "MenuMaintenance";
             this.Text = "MenuMaintenance";
+            ((System.ComponentModel.ISupportInitialize)(this.estandarDataView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,7 +172,7 @@
         private ModifierComponents.EstandarButton estandarButton1;
         private ModifierComponents.EstandarButton estandarButton2;
         private ModifierComponents.EstandarButton estandarButton3;
-        private ModifierComponents.EstandarButton estandarButton4;
         private ModifierComponents.EstandarButton estandarButton5;
+        private ModifierComponents.EstandarDataView estandarDataView1;
     }
 }
