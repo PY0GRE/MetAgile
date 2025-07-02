52188d872477c5bfb6e0092a537e5c0ed5bf7d4d
Bryan Arturo Barraza Garcia <bry.art717@gmail.com>
2025-07-02 10:25:37 -0600

    Clase para alojar la conexion a la base de datos

---
+++ MantenimientoEquipos/MantenimientoEquipos/DataBaseConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MantenimientoEquipos
+{
+    public static class DataBaseConnection
+    {
+        public static string ConnectionString { get; set; } = "Ingresa tu connection String";
+    }
+}
