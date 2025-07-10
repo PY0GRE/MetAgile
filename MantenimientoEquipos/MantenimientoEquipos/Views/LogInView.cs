02bcf5896088c02dace37f1af22f21c3b0e6fd8a
Bryan Arturo Barraza Garcia <bry.art717@gmail.com>
2025-07-09 18:08:37 -0600

    Codigo de funcionalidad Log In con la base de datos

--- MantenimientoEquipos/MantenimientoEquipos/Views/LogInView.cs
+++ MantenimientoEquipos/MantenimientoEquipos/Views/LogInView.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace MantenimientoEquipos.Views
 {
     public partial class LogInView : Form
     {
+
+        SqlConnection conx = new SqlConnection(DataBaseConnection.ConnectionString);
+
         public LogInView()
         {
             InitializeComponent();
@@ -22,8 +27,56 @@ namespace MantenimientoEquipos.Views
 
         private void bttAccept_Click(object sender, EventArgs e)
         {
-            new MainMenuView().Show();
-            this.Hide();
+            ValidarUsuario(txtUser.Text, txtPassword.Text);
+        }
+
+        private void ValidarUsuario(string usuario, string passw)
+        {
+            try
+            {
+                conx.Open();
+
+                SqlCommand consult = new SqlCommand("Select idUser FROM Users Where idUser = @usua and password = @pass and idStatus = 1", conx);
+
+                consult.Parameters.AddWithValue("usua", usuario);
+                consult.Parameters.AddWithValue("pass", passw);
+
+                SqlDataAdapter sd = new SqlDataAdapter(consult);
+
+                DataTable dt = new DataTable();
+
+                sd.Fill(dt);
+
+                // Mesages para verificar
+                //MessageBox.Show(sd.Fill(dt).ToString() + usuario + passw);
+
+                //Console.WriteLine(sd.Fill(dt).ToString());
+
+                if ( dt.Rows.Count == 1 )
+                {
+                    //this.Hide();
+                    String cad = dt.Rows[ 0 ][ 0 ].ToString();
+                    if ( cad == "1" )
+                    {
+                        Console.WriteLine(dt.Rows[ 0 ][ 0 ].ToString());
+                        MainMenuView admin = new MainMenuView();
+                        admin.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Datos Incorrectos");
+                    }
+                }
+            }
+            catch ( Exception e )
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                conx.Close();
+            }
         }
     }
 }
